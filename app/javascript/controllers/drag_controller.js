04d0979d921912c27b2c82f8a558256ecbe88b67
kate gleeson <kate.gleeson@thoughtbot.com>
2021-02-07 13:49:51 +0000

    Allow tile dragging and dropping

---
+++ app/javascript/controllers/drag_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  dragStart(event) {
+    event.dataTransfer.setData('tile', event.target.id)
+    event.target.style.opacity = 0.1
+  }
+
+  dragEnd(event) {
+    event.target.style.opacity = 1
+  }
+
+  drop(event) {
+    event.preventDefault()
+    const data = event.dataTransfer.getData('tile')
+    event.target.appendChild(document.getElementById(data))
+  }
+
+  dragOver(event) {
+    event.preventDefault()
+  }
+}

22910359dcb67713290f87e27473c30d006683e1
kate gleeson <kate.gleeson@thoughtbot.com>
2021-03-21 13:39:19 +0000

    Allow player to shuffle tiles on rack

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
+  static targets = ['space']
+
   dragStart(event) {
     event.dataTransfer.setData('tile', event.target.id)
     event.target.style.opacity = 0.1
@@ -13,11 +15,66 @@ export default class extends Controller {
   drop(event) {
     event.preventDefault()
     const data = event.dataTransfer.getData('tile')
-    const targetChild = event.currentTarget.lastElementChild
+    var tile = document.getElementById(data)
+    var target = event.currentTarget
+    var targetChild = target.lastElementChild
 
     if (!targetChild || !targetChild.classList.value.includes('tile')) {
-      event.currentTarget.appendChild(document.getElementById(data))
+      target.appendChild(tile)
+    } else if (this.isTileRack(target)) {
+      this.insertTile(tile, target, targetChild)
     }
+  }
+
+  insertTile(tile, target, targetChild) {
+    const direction = this.direction(tile, target)
+
+    while (!!tile) {
+      if (!!targetChild) {
+        target.removeChild(targetChild)
+      }
+
+      target.appendChild(tile)
+      tile = targetChild
+      target = this.nextSpace(target, direction)
+
+      if (!!target) {
+        targetChild = target.lastElementChild
+      }
+    }
+  }
+
+  isTileRack(target) {
+    return target.parentElement.classList.value.includes('tile-rack')
+  }
+
+  direction(tile, target) {
+    return this.emptySpacesOnRight(tile, target).length > 0 ? 1 : -1
+  }
+
+
+  emptySpacesOnRight(tile, target) {
+    return this.emptySpaces(tile).filter((space) => {
+      return this.spaceNumber(space) - this.spaceNumber(target) > 0
+    })
+  }
+
+  emptySpaces(tile) {
+    return this.spaceTargets.filter((space) => {
+      return !space.lastElementChild || space.lastElementChild === tile
+    })
+  }
+
+  nextSpace(space, direction) {
+    return this.space(this.spaceNumber(space) + direction)
+  }
+
+  spaceNumber(space) {
+    return parseInt(space.id.split('_')[1])
+  }
+
+  space(num) {
+    return this.spaceTargets.find((space) => space.id == `space_${num}`)
   }
 
   dragOver(event) {

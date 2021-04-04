594e324b464b68dc8c475fdb065d082009740e35
kate gleeson <kate.gleeson@thoughtbot.com>
2021-04-04 18:11:37 +0100

    Add game link to update emails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,9 +16,13 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
   config.action_controller.allow_forgery_protection = false
   config.active_storage.service = :test
-  config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :stderr
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = {
+    host: 'http://www.test.com'
+  }
 end

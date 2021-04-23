e67b45799db74bee7692f9c67d318f01f5f4fdb3
kate gleeson <kate.gleeson@thoughtbot.com>
2021-04-23 18:42:59 +0100

    Add drag and drop for touch screens

---
+++ app/javascript/controllers/touch_controller.js
@@ -0,0 +1,64 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  touchStart(event) {
+    this.moving = event.currentTarget
+    this.movingHeight = event.currentTarget.clientHeight
+    this.movingWidth = event.currentTarget.clientWidth
+    this.moving.style.position = 'fixed';
+  }
+
+  touchMove(event) {
+    this.moving.style.left = `${event.targetTouches[0].clientX - this.movingWidth/2}px`
+    this.moving.style.top = `${event.targetTouches[0].clientY - this.movingHeight/2}px`
+  }
+
+  touchEnd(event) {
+    this.moving.style.zIndex = -10
+    const target = this.target(event)
+
+    if (this.isValidTarget(target)) {
+      target.appendChild(this.moving)
+    }
+
+    this.resetMoving()
+  }
+
+  resetMoving() {
+    this.moving.style.zIndex = 1
+    this.moving.style.left = ''
+    this.moving.style.top = ''
+  }
+
+  target(event) {
+    const changedTouch = event.changedTouches[0]
+
+    const target = document.elementFromPoint(
+      changedTouch.clientX,
+      changedTouch.clientY
+    )
+
+    if (target.classList.value.includes('premium')) {
+      return target.closest('.square')
+    } else {
+      return target
+    }
+  }
+
+  isValidTarget(target) {
+    return (this.isSquare(target) || this.isTileRack(target)) && this.isEmpty(target)
+  }
+
+  isSquare(target) {
+    return target.classList.value.includes('square')
+  }
+
+  isTileRack(target) {
+    return target.classList.value.includes('tile-rack-space')
+  }
+
+  isEmpty(target) {
+    const targetChild = target.lastElementChild
+    return !targetChild || !targetChild.classList.value.includes('tile')
+  }
+}
